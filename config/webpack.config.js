225d54def8e224ad5eb01dc827f7263230d16be6
Andy Lui <awlui2014@gmail.com>
2017-12-14 12:02:07 -0800

    pre boilerplate branch

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 const merge = require('webpack-merge');
-
+const PATHS = {
+  src: path.join(process.cwd(), 'src'),
+  dist: path.join(process.cwd(), 'dist'),
+};
 const commonConfig = merge([
   {
     entry: {
-      index: './'
+      index: './index'
     },
-    context: path.join(process.cwd(), 'src'),,
-    
+    context: path.join(process.cwd(), 'src'),
+    output: {
+      path: PATHS.dist,
+      filename: '[name].js'
+    }
   }
-  ])}
+  ]);
+
+module.exports = () => {
+  return commonConfig;
+};
