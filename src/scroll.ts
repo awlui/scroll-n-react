48df5fc54355de8830351f7616dbf8e4892eb99f
Andy Lui <awlui2014@gmail.com>
2017-12-14 22:00:20 -0800

    Add starter components and wire initial interface

---
+++ src/scroll.ts
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {iScrollProps} from './interfaces/scroll.interface';
+/**
+  ScrollRx React Component
+*/
+export class ScrollRx extends React.Component<iScrollProps, {}> {
+    /**
+    * @param props Props passed from Application to ScrollRx Stateless Component
+    */
+    constructor(props: iScrollProps) {
+      super(props);
+    }
+}
+
