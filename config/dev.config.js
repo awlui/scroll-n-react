165a4413f10a476ffbdaea5f423b5732931def16
Andy Lui <awlui2014@gmail.com>
2017-12-14 16:27:53 -0800

    Change webpack-dev-server to use html-webpack-plugin output

--- config/dev.config.js
+++ config/dev.config.js
@@ -1,16 +1,22 @@
 const merge = require('webpack-merge');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = merge([
 {
   devServer: {
     historyApiFallback: true,
     stats: 'errors-only',
-    contentBase: path.resolve('src', 'public'),
+    contentBase: path.resolve('build'),
     overlay: {
       errors: true,
       warnings: true,
     },
   },
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html'
+      })
+  ]
 }
 
 ]);
